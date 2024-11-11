7e6a2d24825dc838bed04578ac273291844d0e72
Jahir13 <jahir.rocha@epn.edu.ec>
2024-11-11 02:28:39 -0500

    Fin

--- prjJuego/src/Helado.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author      Grupo 6
- * @version     1.0
- * */
-
-public class Helado extends Personaje{
-    public Helado() {
-    }
-}
--- prjJuego/src/Personaje.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author      Grupo 6
- * @version     1.0
- * */
-public class Personaje extends ObjetoJuego {
-}
